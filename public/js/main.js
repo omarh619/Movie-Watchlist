1257e8eddbff53a3d0e2615c95957dd659e97c63
Asantae Shelton <asantaems@gmail.com>
2022-09-02 01:52:07 -0400

    created a function on the main.js page search()

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+
 const deleteBtn = document.querySelectorAll('.del')
 const todoItem = document.querySelectorAll('span.not')
 const todoComplete = document.querySelectorAll('span.completed')
@@ -67,3 +68,40 @@ async function markIncomplete(){
         console.log(err)
     }
 }
+
+//this generates a random number and inputs it into the movie api key
+const movieName = document.querySelector('.movie-name')
+const targetBox = document.querySelector('.target')
+const KEY = 'http://www.omdbapi.com/?apikey=4899398e'
+
+//BELOW DOESNT WORK 
+async function genRan(){
+  let randomMovie = Math.floor(Math.random() * 9000000) + 1000000 
+  let URL = KEY + `&i=tt${randomMovie}`
+    let attempt = await fetch(`${URL}`, {
+        Method: 'POST'
+    }).then((response) => {
+        if(response){
+            console.log(response)
+        }}).catch((error) => console.log(error))
+    
+    return attempt
+}
+//ABOVE DOESNT WORK 
+
+//works perfectly
+async function search(){
+    let randomMovie = Math.floor(Math.random() * 9000000) + 1000000 
+    const response = await fetch(`${KEY}&i=tt${randomMovie}`)
+    const data = await response.json();
+    console.log('Title: ' + data.Title)
+    console.log('Actors: ' + data.Actors)
+    console.log('Year: ' + data.Year)
+    console.log('seriesID: ' + data.seriesID)
+
+};
+
+
+
+
+ 
